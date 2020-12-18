856ad7ff8ae4f41e20b00dd5f170bc0dc3247641
lijie <1363@shinow.com.cn>
2020-12-18 17:15:56 +0800

    动画,添加动画的曲线。

--- lib/demo_list.dart
+++ lib/demo_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_demo/widget/chip/actor_action_chip.dart';
 import 'package:flutter_demo/widget/chip/actor_choice_chip.dart';
 import 'package:flutter_demo/widget/chip/actor_input_chip.dart';
+import 'package:flutter_demo/widget/customLoading/%E4%BD%BF%E7%94%A8AnimatedWidget%E7%AE%80%E5%8C%96/animated_container.dart';
 import 'package:flutter_demo/widget/customLoading/loading_dialog.dart';
 
 import 'custom_widget/chip/wrap_choice_chip/test.dart';
@@ -41,7 +42,8 @@ class _DemoListState extends State<DemoList> {
           RaisedButton(child:Text("ChoiceChipDemo1"),onPressed: ()=>Navigator.push(context, MaterialPageRoute(builder: (BuildContext context)=> ActorChoiceChip()))),
           RaisedButton(child:Text("ActionChipDemo1"),onPressed: ()=>Navigator.push(context, MaterialPageRoute(builder: (BuildContext context)=> ActorActionChip()))),
           RaisedButton(child:Text("SingleChoiceChipTest"),onPressed: ()=>Navigator.push(context, MaterialPageRoute(builder: (BuildContext context)=> SingleChoiceChipTest()))),
-          RaisedButton(child:Text("loading"),onPressed: ()=>Navigator.push(context, MaterialPageRoute(builder: (BuildContext context)=> LoadingDialog()))),
+          RaisedButton(child:Text("animated"),onPressed: ()=>Navigator.push(context, MaterialPageRoute(builder: (BuildContext context)=> LoadingDialog()))),
+          RaisedButton(child:Text("animatedContainer"),onPressed: ()=>Navigator.push(context, MaterialPageRoute(builder: (BuildContext context)=> AnimatedContainerRoute()))),
         ],
       ),
     );
--- lib/widget/customLoading/loading_dialog.dart
+++ lib/widget/customLoading/loading_dialog.dart
@@ -9,32 +9,32 @@ class LoadingDialog extends StatefulWidget {
 }
 
 class _LoadingDialogState extends State<LoadingDialog> with SingleTickerProviderStateMixin {
-  Animation<double> animation;
+  Animation  animation;
   Animation<Color> colorAnimation;
   AnimationController controller;
 
   initState() {
     super.initState();
-    controller = new AnimationController(
-        duration: const Duration(seconds: 3), vsync: this);
+    controller = new AnimationController(duration: const Duration(seconds: 3), vsync: this);
+    //使用弹性曲线
+    animation = CurvedAnimation(parent: controller, curve: Curves.bounceIn);
     //图片宽高从0变到300
-    animation = new Tween(begin: 0.0, end: 300.0).animate(controller)
+    animation = new Tween(begin: 0.0, end: 300.0).animate(animation)
       ..addListener(() {
         setState(() => {});
       });
-    colorAnimation = new ColorTween(begin: Colors.blue,end: Colors.amber).animate(controller)
+    colorAnimation = new ColorTween(begin: Colors.blue, end: Colors.amber).animate(animation)
       ..addListener(() {
         setState(() => {});
       });
-    //启动动画(正向执行)
-
-
   }
+
   dispose() {
     //路由销毁时需要释放动画资源
     controller.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,29 +43,32 @@ class _LoadingDialogState extends State<LoadingDialog> with SingleTickerProvider
       ),
       body: Container(
         child: Column(
-            children: [
-              MaterialButton(
-                  child: Text("Start"),
-                  onPressed: (){
-                    //启动动画(正向执行)
-                    controller.forward();
-                  }),
-              MaterialButton(
-                  child: Text("Stop"),
-                  onPressed: (){
-                controller.stop();
-              }),
-              MaterialButton(
-                  child: Text("Stop"),
-                  onPressed: (){
-                    controller.reverse();
-                  }),
-              Text("Loading"),
-              Container(width: animation.value,height: animation.value,color: colorAnimation.value,)
-      ],
-    ),)
-    ,
+          children: [
+            MaterialButton(
+                child: Text("Start"),
+                onPressed: () {
+                  //启动动画(正向执行)
+                  controller.forward();
+                }),
+            MaterialButton(
+                child: Text("Stop"),
+                onPressed: () {
+                  controller.stop();
+                }),
+            MaterialButton(
+                child: Text("Stop"),
+                onPressed: () {
+                  controller.reverse();
+                }),
+            Text("Loading"),
+            Container(
+              width: animation.value,
+              height: animation.value,
+              color: colorAnimation.value,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
-
