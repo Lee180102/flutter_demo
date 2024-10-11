ec3da6f91127961bb0322a8f3b4be11f111c93cd
lee <jasen58@163.com>
2024-10-11 16:21:15 +0800

    封装倒计时组件。

---
+++ lib/demo/countdown_app/countdown.dart
@@ -0,0 +1,145 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_demo/channel/sound_channel/sound_channel.dart';
+import 'package:flutter_demo/channel/vibration_channel/vibration_channel.dart';
+
+class Countdown extends StatefulWidget {
+  const Countdown(
+      {Key key,
+      @required this.minutes,
+      this.fontSize = 40,
+      this.vibration = false,
+      this.sound = false,
+      this.soundAssetPath})
+      : assert(minutes != null),
+        super(key: key);
+
+  final int minutes;
+  final double fontSize;
+  final bool vibration;
+  final bool sound;
+  final String soundAssetPath;
+
+  @override
+  _CountdownState createState() => _CountdownState();
+}
+
+class _CountdownState extends State<Countdown> {
+  Timer _timer;
+  int _start = 60; // 30分钟 = 30 * 60秒
+
+  @override
+  void initState() {
+    super.initState();
+    this._start = widget.minutes * 60;
+    if (widget.sound &&
+        widget.soundAssetPath != null &&
+        widget.soundAssetPath.length != '') {
+      SoundChannel.initChannels();
+    }
+    if (widget.vibration) {
+      VibrationChannel.initChannels();
+    }
+  }
+
+  void startTimer() {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_start > 1) {
+          _start--;
+        } else {
+          _timer?.cancel();
+          _vibrateAndShowCompletionDialog(); // 倒计时结束后震动并弹窗提醒
+        }
+      });
+    });
+  }
+
+  // 重置倒计时
+  void _resetTimer() {
+    setState(() {
+      _start = widget.minutes * 60; // 重置倒计时为30分钟
+    });
+  }
+
+  // 震动并显示结束提示框，直到用户点击停止震动
+  void _vibrateAndShowCompletionDialog() async {
+    if (widget.sound &&
+        widget.soundAssetPath != null &&
+        widget.soundAssetPath.length != '') {
+      SoundChannel.playSound(widget.soundAssetPath, true);
+    }
+    List<int> pattern = [500, 500, 500];
+    if (widget.vibration) {
+      VibrationChannel.rhythmVibrate(pattern, true);
+    }
+    _showCompletionDialog(); // 弹出提醒框
+  }
+
+  // 停止震动并弹出倒计时结束提示框
+  void _showCompletionDialog() {
+    showDialog(
+      barrierDismissible: false, // 设置为 false 后点击外部不会关闭弹框
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('倒计时结束'),
+          content: Text('倒计时已结束！'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('确定'),
+              onPressed: () async {
+                // 停止震动
+                if (widget.vibration) {
+                  VibrationChannel.stopVibration();
+                }
+                if (widget.sound &&
+                    widget.soundAssetPath != null &&
+                    widget.soundAssetPath.length != '') {
+                  SoundChannel.stopSound();
+                }
+                _resetTimer(); // 点击确定后重置时间
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  // 格式化倒计时为 mm:ss 格式
+  String formatTime(int seconds) {
+    int minutes = seconds ~/ 60;
+    int remainingSeconds = seconds % 60;
+    return '$minutes:${remainingSeconds.toString().padLeft(2, '0')}';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            '倒计时: ${formatTime(_start)}',
+            style: TextStyle(fontSize: widget.fontSize),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              startTimer();
+            },
+            child: Text('开始倒计时'),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/demo/countdown_app/countdown_app.dart
+++ lib/demo/countdown_app/countdown_app.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/channel/sound_channel/sound_channel.dart';
 import 'package:flutter_demo/channel/vibration_channel/vibration_channel.dart';
+import 'package:flutter_demo/demo/countdown_app/countdown.dart';
 
 class CountdownApp extends StatelessWidget {
   @override
@@ -19,82 +20,11 @@ class CountdownPage extends StatefulWidget {
 
 class _CountdownPageState extends State<CountdownPage> {
   Timer _timer;
-  int _start = 1 * 60; // 30分钟 = 30 * 60秒
-  Timer _vibrationTimer;
-
-  @override
-  void initState() {
-    super.initState();
-    SoundChannel.initChannels();
-    VibrationChannel.initChannels();
-  } // 定时震动的 Timer
-
-  void startTimer() {
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {
-        if (_start > 1) {
-          _start--;
-        } else {
-          _timer?.cancel();
-          _vibrateAndShowCompletionDialog(); // 倒计时结束后震动并弹窗提醒
-        }
-      });
-    });
-  }
-
-  // 重置倒计时
-  void _resetTimer() {
-    setState(() {
-      _start = 1 * 60; // 重置倒计时为30分钟
-    });
-  }
-
-  // 震动并显示结束提示框，直到用户点击停止震动
-  void _vibrateAndShowCompletionDialog() async {
-    SoundChannel.playSound('assets/sound/InspiringEpicCinematic.mp3', true);
-    List<int> pattern = [500, 500, 500];
-    VibrationChannel.rhythmVibrate(pattern, true);
-    _showCompletionDialog(); // 弹出提醒框
-  }
-
-  // 停止震动并弹出倒计时结束提示框
-  void _showCompletionDialog() {
-    showDialog(
-      barrierDismissible: false, // 设置为 false 后点击外部不会关闭弹框
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('倒计时结束'),
-          content: Text('倒计时已结束！'),
-          actions: <Widget>[
-            TextButton(
-              child: Text('确定'),
-              onPressed: () async {
-                // 停止震动
-                VibrationChannel.stopVibration();
-                SoundChannel.stopSound();
-                _resetTimer(); // 点击确定后重置时间
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
 
   @override
   void dispose() {
     _timer?.cancel();
-    _vibrationTimer?.cancel(); // 停止震动循环
     super.dispose();
-  }
-
-  // 格式化倒计时为 mm:ss 格式
-  String formatTime(int seconds) {
-    int minutes = seconds ~/ 60;
-    int remainingSeconds = seconds % 60;
-    return '$minutes:${remainingSeconds.toString().padLeft(2, '0')}';
   }
 
   @override
@@ -107,17 +37,13 @@ class _CountdownPageState extends State<CountdownPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              '倒计时: ${formatTime(_start)}',
-              style: TextStyle(fontSize: 40),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                startTimer();
-              },
-              child: Text('开始倒计时'),
-            ),
+            Countdown(
+              minutes: 1,
+              fontSize: 20,
+              vibration: true,
+              sound: true,
+              soundAssetPath: 'assets/sound/InspiringEpicCinematic.mp3',
+            )
           ],
         ),
       ),
